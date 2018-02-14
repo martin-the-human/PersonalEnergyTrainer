4158fc4244d398b6fd605e51fc1a9f4ee78e3fdc
Martin <martinwjwilson@gmail.com>
2018-02-14 15:48:46 +0000

    created button functionality on login screen to take uesr to main page. does not verify details yet.

--- app/src/main/java/com/example/martin/personalenergytrainer/LoginScreen.java
+++ app/src/main/java/com/example/martin/personalenergytrainer/LoginScreen.java
@@ -1,7 +1,10 @@
 package com.example.martin.personalenergytrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button btn_login = (Button) findViewById(R.id.buttonLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainActivity = new Intent(LoginScreen.this, MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
     }
 }
