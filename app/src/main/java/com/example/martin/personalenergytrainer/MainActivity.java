a2437c3c82d408532e4a6104f4e7b57c8f25544a
Martin <martinwjwilson@gmail.com>
2018-02-15 13:14:37 +0000

    navigation buttons on the main screen now take the user to the corresponding screen

--- app/src/main/java/com/example/martin/personalenergytrainer/MainActivity.java
+++ app/src/main/java/com/example/martin/personalenergytrainer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.martin.personalenergytrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnProfile = (Button) findViewById(R.id.btnNavigationProfile);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profile = new Intent(MainActivity.this, UserProfile.class);
+                startActivity(profile);
+            }
+        });
+
+        Button btnStats = (Button) findViewById(R.id.btnNavigationStats);
+        btnStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent stats = new Intent(MainActivity.this, Stats.class);
+                startActivity(stats);
+            }
+        });
+
+        Button btnSettings = (Button) findViewById(R.id.btnNavigationSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settings = new Intent(MainActivity.this, Settings.class);
+                startActivity(settings);
+            }
+        });
     }
 }
