1f3d1af000657ced0eadb299b90db9139ba00523
Martin <martinwjwilson@gmail.com>
2018-03-13 23:30:07 +0000

    working on button functionality

--- app/src/main/java/com/example/martin/personalenergytrainer/Settings.java
+++ app/src/main/java/com/example/martin/personalenergytrainer/Settings.java
@@ -3,17 +3,19 @@ package com.example.martin.personalenergytrainer;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 
-public class Settings extends ListActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        String[] listOfSettings = {"Colour Blind Mode", "12 Hour Time Format", "24 Hour Time Format"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, listOfSettings);
-        getListView().setAdapter(adapter);
+        RadioButton radio12 = findViewById(R.id.rad12hr);
+        final RadioButton radio24 = findViewById(R.id.rad24hr);
+        
     }
 }
