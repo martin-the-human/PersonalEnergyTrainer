69e8484be2fbcc3710f1ec6476bdcb2eb02548a0
Martin <martinwjwilson@gmail.com>
2018-04-10 11:20:34 +0100

    login screen edit

--- app/src/main/java/com/example/martin/personalenergytrainer/LoginScreen.java
+++ app/src/main/java/com/example/martin/personalenergytrainer/LoginScreen.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.net.URL;
+
 public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        URL url = new URL("146.176.251.40");
 
         Button btn_login = (Button) findViewById(R.id.buttonLogin);
         btn_login.setOnClickListener(new View.OnClickListener() {
